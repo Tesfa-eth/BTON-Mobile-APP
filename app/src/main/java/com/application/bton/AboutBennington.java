de25a4ad5ed83a92145c6ebef82ad5498d18bf3d
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-10-30 14:30:51 -0400

    BTON andriod mobile APP - small updates - added ABOUT Bennington

---
+++ app/src/main/java/com/application/bton/AboutBennington.java
@@ -0,0 +1,14 @@
+package com.application.bton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutBennington extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_bennington);
+    }
+}
--- app/src/main/java/com/application/bton/Mainpage.java
+++ app/src/main/java/com/application/bton/Mainpage.java
@@ -2,13 +2,40 @@ package com.application.bton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Mainpage extends AppCompatActivity {
+public class Mainpage extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.btnGallery:
+                Intent intent = new Intent(Mainpage.this, Log.class);
+                startActivity(intent);
+                break;
+            case  R.id.btnAboutBTN:
+                Intent aboutBtn_intent = new Intent(Mainpage.this, AboutBennington.class);
+                startActivity(aboutBtn_intent);
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        Button btnGallery = findViewById(R.id.btnGallery);
+        btnGallery.setOnClickListener(this);
+
+        Button btnAboutBTN = findViewById(R.id.btnAboutBTN);
+        btnAboutBTN.setOnClickListener(this);
     }
 }
