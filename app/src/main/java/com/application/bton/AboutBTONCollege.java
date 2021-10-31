b8e14e595c43d6409c68ebe0df66c9612c4eb5e9
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-10-30 20:46:08 -0400

    attached a url ("https://www.bennington.edu") to the button

--- app/src/main/java/com/application/bton/AboutBTONCollege.java
+++ app/src/main/java/com/application/bton/AboutBTONCollege.java
@@ -2,7 +2,10 @@ package com.application.bton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutBTONCollege extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class AboutBTONCollege extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_btoncollege);
+        findViewById(R.id.btnofficialWebsite).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("https://www.bennington.edu/");
+            }
+        });
+    }
+
+    public void clicked_btn(String url){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
