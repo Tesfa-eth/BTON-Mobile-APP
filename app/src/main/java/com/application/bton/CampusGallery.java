1ca6aeeea95e0d012c37fb0db4ed141b1a5087f1
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-11-12 19:59:59 -0500

    Making sure that every change has been pushed.

--- app/src/main/java/com/application/bton/CampusGallery.java
+++ app/src/main/java/com/application/bton/CampusGallery.java
@@ -3,12 +3,35 @@ package com.application.bton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.Button;
 
 public class CampusGallery extends AppCompatActivity {
+
+    Button minimize;
+    View toMinimize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_gallery);
+
+        minimize = findViewById(R.id.minimize);
+        toMinimize = findViewById(R.id.exampleImages);
+
+        minimize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(toMinimize.getVisibility() == View.GONE){
+                    toMinimize.setVisibility(View.VISIBLE);
+                }
+                else{
+                    toMinimize.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/application/bton/HealthCenter.java
+++ app/src/main/java/com/application/bton/HealthCenter.java
@@ -18,6 +18,7 @@ public class HealthCenter extends AppCompatActivity {
     ConstraintLayout expandableview;
     Button downarrow;
     CardView information;
+    CardView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +28,21 @@ public class HealthCenter extends AppCompatActivity {
         expandableview = findViewById(R.id.expandableView);
         downarrow = findViewById(R.id.downarrow);
         information = findViewById(R.id.information);
+        location = findViewById(R.id.location);
 
         downarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (expandableview.getVisibility() == View.GONE){
                     TransitionManager.beginDelayedTransition(information, new AutoTransition());
+                    TransitionManager.beginDelayedTransition(location, new AutoTransition());
                     expandableview.setVisibility(View.VISIBLE);
                     downarrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
                 }
                 else{
-                    TransitionManager.beginDelayedTransition(information, new AutoTransition());
+                    //this code is commented in order to avoid overlapping
+                    //TransitionManager.beginDelayedTransition(information, new AutoTransition());
+                    TransitionManager.beginDelayedTransition(location, new AutoTransition());
                     expandableview.setVisibility(View.GONE);
                     downarrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
                 }
