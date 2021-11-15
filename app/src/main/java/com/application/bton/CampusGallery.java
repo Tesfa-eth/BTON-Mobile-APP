c1f113b8ac991e7a5693728902d496bb26916f29
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-11-15 09:59:52 -0500

    Campus gallery view updated. Good but still some bugs to fix.

--- app/src/main/java/com/application/bton/CampusGallery.java
+++ app/src/main/java/com/application/bton/CampusGallery.java
@@ -12,8 +12,8 @@ import android.widget.Button;
 
 public class CampusGallery extends AppCompatActivity {
 
-    Button minimize;
-    CardView toMinimize;
+    Button minimize, minimize2;
+    CardView toMinimize, toMinimize2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,9 @@ public class CampusGallery extends AppCompatActivity {
 
         minimize = findViewById(R.id.minimize);
         toMinimize = findViewById(R.id.exampleImages);
+
+        minimize2 = findViewById(R.id.buttonDown);
+        toMinimize2 = findViewById(R.id.vapaCardView);
 
         minimize.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +37,21 @@ public class CampusGallery extends AppCompatActivity {
                 else{
                     toMinimize.setVisibility(View.GONE);
                     minimize.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
+                }
+            }
+        });
+
+        minimize2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(toMinimize2.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(toMinimize2, new AutoTransition());
+                    toMinimize2.setVisibility(View.VISIBLE);
+                    minimize2.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
+                }
+                else{
+                    toMinimize2.setVisibility(View.GONE);
+                    minimize2.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
                 }
             }
         });
