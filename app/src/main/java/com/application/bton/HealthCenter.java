27aa6ae324ad3c1a671e248655d68a1bd7a946e5
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-11-17 11:24:59 -0500

    Added a second card card view and trying a different graphics!

--- app/src/main/java/com/application/bton/HealthCenter.java
+++ app/src/main/java/com/application/bton/HealthCenter.java
@@ -15,36 +15,60 @@ import java.util.ArrayList;
 
 public class HealthCenter extends AppCompatActivity {
 
-    ConstraintLayout expandableview;
-    Button downarrow;
-    CardView information;
-    CardView location;
+    ConstraintLayout expinformation, expScheduleApt;
+    Button btnInformation, btnscheduleAptm;
+    CardView information, scheduleaptm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_center);
 
-        expandableview = findViewById(R.id.expandableView);
-        downarrow = findViewById(R.id.downarrow);
+        //general information
+        expinformation = findViewById(R.id.expInformation);
+        btnInformation = findViewById(R.id.btnInformation);
         information = findViewById(R.id.information);
-        location = findViewById(R.id.location);
 
-        downarrow.setOnClickListener(new View.OnClickListener() {
+        //schedule appointment
+        expScheduleApt = findViewById(R.id.expScheduleAptm);
+        btnscheduleAptm = findViewById(R.id.btnscheduleAptm);
+        scheduleaptm = findViewById(R.id.scheduleaptm);
+
+
+        btnInformation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (expandableview.getVisibility() == View.GONE){
+                if (expinformation.getVisibility() == View.GONE){
                     TransitionManager.beginDelayedTransition(information, new AutoTransition());
-                    TransitionManager.beginDelayedTransition(location, new AutoTransition());
-                    expandableview.setVisibility(View.VISIBLE);
-                    downarrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
+                    TransitionManager.beginDelayedTransition(scheduleaptm, new AutoTransition());
+                    expinformation.setVisibility(View.VISIBLE);
+                    btnInformation.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
                 }
                 else{
                     //this code is commented in order to avoid overlapping
                     //TransitionManager.beginDelayedTransition(information, new AutoTransition());
-                    TransitionManager.beginDelayedTransition(location, new AutoTransition());
-                    expandableview.setVisibility(View.GONE);
-                    downarrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
+                    TransitionManager.beginDelayedTransition(scheduleaptm, new AutoTransition());
+                    expinformation.setVisibility(View.GONE);
+                    btnInformation.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
+                }
+            }
+        });
+
+        btnscheduleAptm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expScheduleApt.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(scheduleaptm, new AutoTransition());
+                    //TransitionManager.beginDelayedTransition(information, new AutoTransition()); may be get the next cardView id?
+                    expScheduleApt.setVisibility(View.VISIBLE);
+                    btnscheduleAptm.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
+                }
+                else{
+                    //this code is commented in order to avoid overlapping
+                    //TransitionManager.beginDelayedTransition(information, new AutoTransition());
+                    // TransitionManager.beginDelayedTransition(scheduleaptm, new AutoTransition()); new view?
+                    expScheduleApt.setVisibility(View.GONE);
+                    btnscheduleAptm.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
                 }
             }
         });
