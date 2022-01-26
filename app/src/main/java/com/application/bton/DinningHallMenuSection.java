b6f90aa20cd8f0dd4bdaba2e80f2dc6c53ef4c9f
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-01-26 17:38:32 -0500

    Menu updating working.

--- app/src/main/java/com/application/bton/DinningHallMenuSection.java
+++ app/src/main/java/com/application/bton/DinningHallMenuSection.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.backendless.Backendless;
@@ -16,39 +20,73 @@ import java.util.List;
 
 public class DinningHallMenuSection extends AppCompatActivity {
 
-    Button getMeal;
+    TextView foodlist, dayView;
+    public final String[] day = {"Monday"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dinning_hall_menu_section);
 
-        getMeal = findViewById(R.id.btnGetmeal);
-        getMeal.setOnClickListener(new View.OnClickListener() {
+        // day of the day of the week spinner
+        Spinner spinnerDay = (Spinner) findViewById(R.id.spinnerDay);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterDay = ArrayAdapter.createFromResource(this,
+                R.array.dayOfTheWeek, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterDay.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerDay.setAdapter(adapterDay);
+        // Day spinner item selected listener
+        spinnerDay.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                String whereClause = "day = 'Monday'";
-                DataQueryBuilder queryBuilder = DataQueryBuilder.create();
-                queryBuilder.setWhereClause(whereClause);
-                queryBuilder.setPageSize(100).setOffset(0);
-                queryBuilder.setSortBy("day");
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                day[0] = parent.getItemAtPosition(position).toString();
+                toastMessage(day[0]);
+                readDataAndDisplay();
+            }
 
-                Backendless.Persistence.of(DhallMenu.class).find(queryBuilder,new AsyncCallback<List<DhallMenu>>() {
-                    @Override
-                    public void handleResponse(List<DhallMenu> response) {
-                        toastMessage("count: " + response.size());
-                        for(int i=0; i < response.size(); i++){
-                            toastMessage(response.get(i).getDay() + " - " +
-                                    response.get(i).getMealOfTheDay()+ " - " + response.get(i).getMeal());
-                        }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // error handling needed?
+                toastMessage("Nothing selected yet!");
+            }
+        });
 
-                    }
+    }
+    public void readDataAndDisplay(){
+        String whereClause = "day = '" + day[0] + "'";
+        DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+        queryBuilder.setWhereClause(whereClause);
+        queryBuilder.setPageSize(100).setOffset(0);
+        queryBuilder.setSortBy("day");
 
-                    @Override
-                    public void handleFault(BackendlessFault fault) {
+        Backendless.Persistence.of(DhallMenu.class).find(queryBuilder,new AsyncCallback<List<DhallMenu>>() {
+            @Override
+            public void handleResponse(List<DhallMenu> response) {
+                toastMessage("count: " + response.size());
+                for(int i=0; i < response.size(); i++){
+                    toastMessage(response.get(i).getDay() + " - " +
+                            response.get(i).getMealOfTheDay()+ " - " + response.get(i).getMeal());
+                }
+                foodlist = findViewById(R.id.foodlist);
+                dayView = findViewById(R.id.dayView);
 
-                        toastMessage("fault");
-                    }
-                });
+                dayView.setText(day[0]);
+                if (response.size() > 0){
+                    foodlist.setText(response.get(0).getMeal());
+                    //foodlist.setText(response.get(0).getDay() + " - " + response.get(0).getMealOfTheDay()+ " - " + response.get(0).getMeal());
+                }
+                else{
+                    foodlist.setText("Sorry the menu is not updated yet!");
+                }
+
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+                toastMessage("fault");
             }
         });
     }
