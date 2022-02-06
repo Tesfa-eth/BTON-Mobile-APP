4ddeda30177a8da8ef9c4abf44826414b25352a2
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-02-05 23:35:38 -0500

    Added by category search.
    * Also work with Images

--- app/src/main/java/com/application/bton/CampusGallery.java
+++ app/src/main/java/com/application/bton/CampusGallery.java
@@ -9,10 +9,13 @@ import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,22 +32,62 @@ public class CampusGallery extends AppCompatActivity {
 
     //declaring variables
     ImageView sampleImage;
-    Button sampleButton;
-    //public final ArrayList result = new ArrayList<List<gallery>>();
+    Button btnGeneralCampusView, btnSearchCatagory;
     public final List<gallery>[] result = new List[]{new ArrayList<>()};
+    public final List<GalleryCatagories>[] resultCategory = new List[]{new ArrayList<>()};
+    //public final ArrayList<List<GalleryCatagories>> arrayCategory = new ArrayList<>();
+    public final String[] spinnerStorage = {"galleryCatagory"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_gallery);
 
-        sampleButton = findViewById(R.id.sampleButton);
-        sampleButton.setOnClickListener(new View.OnClickListener() {
+        // get spinner
+        Spinner galleryCategorySpinner = (Spinner) findViewById(R.id.spinnerGalleryCategory);
+        ArrayAdapter<CharSequence> adapterGalleryCategory = ArrayAdapter.createFromResource(this,
+                R.array.galleryCategories, android.R.layout.simple_spinner_item);
+        adapterGalleryCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        galleryCategorySpinner.setAdapter(adapterGalleryCategory);
+        // get everything from the GalleryCatagories table and store it in resultCategory
+        //getALlCatagories();
+        galleryCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCategory = parent.getItemAtPosition(position).toString();
+                if(selectedCategory.equals("Categories")){
+                    return;
+                }
+                else{
+                    spinnerStorage[0] = selectedCategory;
+                    getByCatagory();
+                    btnGeneralCampusView.setText("General Campus View");
+                }
+                //toastMessage(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //btn search category
+        btnSearchCatagory = findViewById(R.id.btnSearchCatagory);
+        btnSearchCatagory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadCatagory();
+            }
+        });
+
+        btnGeneralCampusView = findViewById(R.id.btnGeneralCampusView);
+        btnGeneralCampusView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadMore();
-                toastMessage("loading...");
-                sampleButton.setText("Next page");
+                toastMessage("Next page");
+                btnGeneralCampusView.setText("Next page");
 
             }
         });
@@ -67,7 +110,8 @@ public class CampusGallery extends AppCompatActivity {
                     queryBuilder.prepareNextPage();
                     Backendless.Data.of(gallery.class).find(queryBuilder, this);
                 } else {
-                    toastMessage("Start time" + (System.currentTimeMillis() - startTime));
+                    return;
+                    //toastMessage("Start time" + (System.currentTimeMillis() - startTime));
                 }
             }
 
@@ -76,6 +120,44 @@ public class CampusGallery extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void loadCatagory(){
+        // calls adapter view
+        //toastMessage(spinnerStorage[0] + "got" + resultCategory[0].get(0).getPhoto_url());
+        ListView mListView = findViewById(R.id.imageListView);
+        MyCatagoryAdapter myCatagoryAdapter = new MyCatagoryAdapter();
+        mListView.setAdapter(myCatagoryAdapter);
+    }
+
+    public void getByCatagory(){
+        GalleryCatagories galleryCatagories = new GalleryCatagories();
+        final int PAGESIZE = 100;
+        final Integer[] currentPage = {0};
+        DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+        String whereclause = "title = '" + spinnerStorage[0] + "'";
+        queryBuilder.setWhereClause(whereclause);
+        queryBuilder.setPageSize(PAGESIZE);
+        Backendless.Persistence.of(GalleryCatagories.class).find(queryBuilder, new AsyncCallback<List<GalleryCatagories>>() {
+            @Override
+            public void handleResponse(List<GalleryCatagories> response) {
+                resultCategory[0]=response;
+                int size = response.size();
+                if (size == PAGESIZE) {
+                    queryBuilder.prepareNextPage();
+                    Backendless.Data.of(GalleryCatagories.class).find(queryBuilder, this);
+                } else {
+                    return;
+                    //("Start time" + (System.currentTimeMillis() - startTime));
+                }
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+
     }
 
     public void loadMore(){
@@ -107,6 +189,36 @@ public class CampusGallery extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             convertView = getLayoutInflater().inflate(R.layout.card_gallery_images, parent, false);
             // display the image
+            ImageView profileImage = convertView.findViewById(R.id.cardViewGalleryImage);
+            Glide.with(CampusGallery.this).load(response.get(position).getPhoto_url()).into(profileImage);
+            return convertView;
+        }
+    }
+
+    public class MyCatagoryAdapter extends BaseAdapter {
+
+        List<GalleryCatagories> response = resultCategory[0];
+
+        @Override
+        public int getCount() {
+            return response.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.card_gallery_images, parent, false);
+            // display the image
+            //toastMessage("arrayResult: "+ (arrayCategory.size()*arrayCategory.get(0).size()));
             ImageView profileImage = convertView.findViewById(R.id.cardViewGalleryImage);
             Glide.with(CampusGallery.this).load(response.get(position).getPhoto_url()).into(profileImage);
             return convertView;
